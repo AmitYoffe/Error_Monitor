cd17d7c3cd6b86e14175db4fb897ae5816a4f3c7
AmitYoffe <yoffe.amit@gmail.com>
2024-04-10 14:42:26 +0300

    -

--- src/components/DevInfo/PageToggle.tsx
+++ src/components/DevInfo/PageToggle.tsx
@@ -1,21 +1,34 @@
 import { TrafficCone } from 'lucide-react';
 import { ScrollText } from 'lucide-react';
-
+import { useLocation } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { Button } from '../ui/button';
 
 export default function PageToggle() {
+  const location = useLocation();
+  const pathnames = location.pathname.split('/').filter((x) => x);
+
   const navigation = useNavigate();
 
+  let navigateTo;
+  let toggleIcon = <TrafficCone />;
+
+  if (pathnames.includes('info-dev')) {
+    toggleIcon = <ScrollText />;
+    navigateTo = '/';
+  } else {
+    toggleIcon = <TrafficCone />;
+    navigateTo = '/info-dev';
+  }
+
   const handleClick = () => {
-    navigation('/info-dev');
+    navigation(navigateTo);
   };
-  // TODO: make this check the current page (according to URL or state?)
-  const trueCondition = true;
+
   return (
     <div className="absolute right-40 top-1">
       <Button variant="outline" size="icon" onClick={handleClick}>
-        {trueCondition === true ? <TrafficCone /> : <ScrollText />}
+        {toggleIcon}
       </Button>
     </div>
   );
