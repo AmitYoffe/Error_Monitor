bc62c807d1555357238b8bd9fd92f7765fb137a2
AmitYoffe <yoffe.amit@gmail.com>
2024-05-08 09:15:32 +0300

    Unfinished skeleton added

--- src/components/mainDashboard/NetworkChoice.tsx
+++ src/components/mainDashboard/NetworkChoice.tsx
@@ -1,8 +1,9 @@
-import { getDashboardInfo } from "@/lib/getDashboardInfo";
-import moment from "moment";
-import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { Card, CardDescription, CardTitle } from "../ui/card";
+import { getDashboardInfo } from '@/lib/getDashboardInfo';
+import moment from 'moment';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Card, CardDescription, CardTitle } from '../ui/card';
+import { Skeleton } from '../ui/skeleton';
 
 interface NetworkChoiceProps {
   location: string;
@@ -10,22 +11,40 @@ interface NetworkChoiceProps {
   title: string;
 }
 
-
-
-export default function NetworkChoice({ location, statusColor, title, }: NetworkChoiceProps) {
+export default function NetworkChoice({
+  location,
+  statusColor,
+  title,
+}: NetworkChoiceProps) {
   const [networkHovered, setNetworkHovered] = useState(false);
   const [totalDocs, setTotalDocs] = useState<number | string | null>(null);
-  const [docs3DaysAgo, setDocs3DaysAgo] = useState<number | string | null>(null);
-  const [lastTimeRecieved, setLastTimeRecieved] = useState<number | string | null>(null);
+  const [docs3DaysAgo, setDocs3DaysAgo] = useState<number | string | null>(
+    null,
+  );
+  const [lastTimeRecieved, setLastTimeRecieved] = useState<
+    number | string | null
+  >(null);
 
-  const urlArgument = title === 'Agencies' ? 'ag' : 'sn'
+  const urlArgument = title === 'Agencies' ? 'ag' : 'sn';
 
   useEffect(() => {
     const fetchDocsCount = async () => {
       try {
-        const totalDocs = await getDashboardInfo({ location, docInfoField: 'docs_count', networkType: 'sn' });
-        const docs3DaysAgo = await getDashboardInfo({ location, docInfoField: 'docs_count_3_days', networkType: 'sn' });
-        const lastTimeRecieved = await getDashboardInfo({ location, docInfoField: 'last_time', networkType: 'sn' });
+        const totalDocs = await getDashboardInfo({
+          location,
+          docInfoField: 'docs_count',
+          networkType: 'sn',
+        });
+        const docs3DaysAgo = await getDashboardInfo({
+          location,
+          docInfoField: 'docs_count_3_days',
+          networkType: 'sn',
+        });
+        const lastTimeRecieved = await getDashboardInfo({
+          location,
+          docInfoField: 'last_time',
+          networkType: 'sn',
+        });
         // const locationInfo = await getLocationInfo("")
         // const {totalDocs,
         //   docs3DaysAgo,
@@ -33,7 +52,6 @@ export default function NetworkChoice({ location, statusColor, title, }: Network
         setTotalDocs(totalDocs);
         setDocs3DaysAgo(docs3DaysAgo);
         setLastTimeRecieved(lastTimeRecieved);
-
       } catch (error) {
         console.error('Error fetching docs:', error);
         // If there's an error, set the state with 'no information'
@@ -49,28 +67,30 @@ export default function NetworkChoice({ location, statusColor, title, }: Network
   return (
     <Link to={`${location}/${urlArgument}`}>
       <Card
-        className='flex-col p-2'
+        className="flex-col p-2"
         style={{
           border: networkHovered ? `1px solid ${statusColor}` : undefined,
         }}
         onMouseOver={() => setNetworkHovered(true)}
         onMouseOut={() => setNetworkHovered(false)}
       >
-        <CardTitle className="p-2 text-center underline">
-          {title}
-        </CardTitle>
+        <CardTitle className="p-2 text-center underline">{title}</CardTitle>
         <CardDescription className="mx-12 flex justify-between">
-          Last Time Recieved: <div>{moment(lastTimeRecieved).format('DD/MM/YYYY - HH:mm:ss')}</div>
+          Last Time Recieved:{' '}
+          <div>{moment(lastTimeRecieved).format('DD/MM/YYYY - HH:mm:ss')}</div>
         </CardDescription>
         <CardDescription className="mx-12 flex justify-between">
           Docs from 3 days ago: <div>{docs3DaysAgo}</div>
         </CardDescription>
         <CardDescription className="mx-12 flex justify-between">
-          Total Docs: <div>{totalDocs}</div>
+          Total Docs:
+          {/* Skeleton that is shown when the info takes time to load */}
+          <Skeleton className="my-auto h-2 w-[90px]" />
+          {/* <div>{totalDocs}</div> */}
         </CardDescription>
       </Card>
     </Link>
-  )
+  );
 }
 
 // TODO: Add skeleton onload, because it takes a long time to load
