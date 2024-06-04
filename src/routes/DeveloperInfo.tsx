ff42932a0137924583d82c5607808d98190dacd4
Donald Ressler <ronald_ressler1@proton.me>
2024-06-04 20:47:10 +0300

    -

--- src/routes/DeveloperInfo.tsx
+++ src/routes/DeveloperInfo.tsx
@@ -1,56 +1,56 @@
 import shyDuck from '@/assets/images/shy-duck.png';
-import TrafficLightsDisplay from '@/components/DevInfo/TrafficLightsDisplay';
 import { CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
-import { ToolCategory } from '@/types/statusType';
-import {
-  DatabaseIcon,
-  DownloadIcon,
-  SearchIcon,
-  SparklesIcon,
-} from 'lucide-react';
+// import TrafficLightsDisplay from '@/components/DevInfo/TrafficLightsDisplay';
+// import { ToolCategory } from '@/types/statusType';
+// import {
+//   DatabaseIcon,
+//   DownloadIcon,
+//   SearchIcon,
+//   SparklesIcon,
+// } from 'lucide-react';
 
-interface IToolsCollectionJson {
-  [key: string]: ToolCategory;
-}
+// interface IToolsCollectionJson {
+//   [key: string]: ToolCategory;
+// }
 
 export default function DeveloperInfo() {
-  const toolsCollectionJson: IToolsCollectionJson = {
-    Analyzing: {
-      icon: <SearchIcon className="h-5 w-5" />,
-      tools: {
-        Ingestion: 'operational',
-        Intuview: 'operational',
-        Siren: 'operational',
-      },
-    },
-    Collecting: {
-      icon: <DownloadIcon className="h-5 w-5" />,
-      tools: {
-        'FB groups': 'operational',
-        'FB pages': 'operational',
-        'FB profiles': 'unstable',
-        'TG channels': 'operational',
-        'TW users': 'operational',
-        Voyager: 'operational',
-      },
-    },
-    Enrichment: {
-      icon: <SparklesIcon className="h-5 w-5" />,
-      tools: {
-        Translation: 'operational',
-        Embedding: 'operational',
-        Clustering: 'operational',
-        Relations: 'operational',
-      },
-    },
-    Databases: {
-      icon: <DatabaseIcon className="h-5 w-5" />,
-      tools: {
-        Mongo: 'operational',
-        Elastic: 'operational',
-      },
-    },
-  };
+  // const toolsCollectionJson: IToolsCollectionJson = {
+  //   Analyzing: {
+  //     icon: <SearchIcon className="h-5 w-5" />,
+  //     tools: {
+  //       Ingestion: 'operational',
+  //       Intuview: 'operational',
+  //       Siren: 'operational',
+  //     },
+  //   },
+  //   Collecting: {
+  //     icon: <DownloadIcon className="h-5 w-5" />,
+  //     tools: {
+  //       'FB groups': 'operational',
+  //       'FB pages': 'operational',
+  //       'FB profiles': 'unstable',
+  //       'TG channels': 'operational',
+  //       'TW users': 'operational',
+  //       Voyager: 'operational',
+  //     },
+  //   },
+  //   Enrichment: {
+  //     icon: <SparklesIcon className="h-5 w-5" />,
+  //     tools: {
+  //       Translation: 'operational',
+  //       Embedding: 'operational',
+  //       Clustering: 'operational',
+  //       Relations: 'operational',
+  //     },
+  //   },
+  //   Databases: {
+  //     icon: <DatabaseIcon className="h-5 w-5" />,
+  //     tools: {
+  //       Mongo: 'operational',
+  //       Elastic: 'operational',
+  //     },
+  //   },
+  // };
 
   return (
     <div>
@@ -58,11 +58,11 @@ export default function DeveloperInfo() {
         <img src={shyDuck} alt="shy traffic cone duck" className="h-20 w-16" />
         <CardTitle>Dev Error Monitor</CardTitle>
         <CardDescription>
-          Lead your quest to find the errors.
-          {/* This area is still under construction... */}
+          {/* Lead your quest to find the errors. */}
+          This area is still under construction...
         </CardDescription>
       </CardHeader>
-      <TrafficLightsDisplay toolsCollectionJson={toolsCollectionJson} />
+      {/* <TrafficLightsDisplay toolsCollectionJson={toolsCollectionJson} /> */}
     </div>
   );
 }
